7328167576c00f00aa14876448f9769d1f8636b2
Avinash Dalvi <avinashsdalvi@gmail.com>
2024-11-19 10:33:54 +0530

    Added loadbalancer

--- Mautic/init.sql
+++ Mautic/init.sql
@@ -1,5 +1,15 @@
 CREATE DATABASE IF NOT EXISTS mautic;
+
+-- Attempt to drop the user if it already exists
+DROP USER IF EXISTS 'mautic'@'%';
+
+-- Recreate the user
 CREATE USER 'mautic'@'%' IDENTIFIED BY 'mautic_password';
-ALTER USER 'mautic' IDENTIFIED WITH mysql_native_password BY 'mautic_password';
+
+-- Set the password using native password authentication
+ALTER USER 'mautic'@'%' IDENTIFIED WITH mysql_native_password BY 'mautic_password';
+
+-- Grant privileges
 GRANT ALL PRIVILEGES ON mautic.* TO 'mautic'@'%';
+
 FLUSH PRIVILEGES;
